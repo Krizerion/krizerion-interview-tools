020c10b404a8436969cc5653596b5daae3ecd38d
Krizerion <kris.petrov.93@gmail.com>
2024-11-17 19:19:04 +0200

    prime theme added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,46 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputTextModule } from 'primeng/inputtext';
+import { BrowserModule } from '@angular/platform-browser';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { DropdownModule } from 'primeng/dropdown';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'kit-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [
+    InputTextModule,
+    CheckboxModule,
+    ButtonModule,
+    RadioButtonModule,
+    InputTextareaModule,
+    DropdownModule,
+    FormsModule,
+    CommonModule,
+  ],
 })
 export class AppComponent {
-  title = 'krizerion-interview-tools';
+  selectedState: any = null;
+
+  states: any[] = [
+    { name: 'Arizona', code: 'Arizona' },
+    { name: 'California', value: 'California' },
+    { name: 'Florida', code: 'Florida' },
+    { name: 'Ohio', code: 'Ohio' },
+    { name: 'Washington', code: 'Washington' },
+  ];
+
+  cities1: any[] = [];
+
+  cities2: any[] = [];
+
+  city1: any = null;
+
+  city2: any = null;
 }
